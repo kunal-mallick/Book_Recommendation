9a826511eecc94eefece6ac2b4138c036f1db704
Kunal Mallick <kunalmallicks3@gmail.com>
2023-05-25 14:47:42 +0530

    Creating Data frame

---
+++ main.py
@@ -0,0 +1,9 @@
+import pickle
+import pandas as pd
+import streamlit as st
+
+st.markdown('<img src ="https://images-na.ssl-images-amazon.com/images/P/0002005018.0" ></img>', unsafe_allow_html=True)
+
+st.markdown('<img src ="https://images-na.ssl-images-amazon.com/images/P/0195153448.0" ></img>', unsafe_allow_html=True)
+
+st.markdown('<img src ="https://images-na.ssl-images-amazon.com/images/P/0002005018.0" ></img>', unsafe_allow_html=True)
