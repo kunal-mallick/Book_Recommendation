52d82999ad1743537d7d98077081b9a88e9f0697
Kunal Mallick <kunalmallicks3@gmail.com>
2023-06-01 14:27:33 +0530

    Started Deployment
    - added header
    
    - added input variable

--- main.py
+++ main.py
@@ -2,8 +2,49 @@ import pickle
 import pandas as pd
 import streamlit as st
 
-st.markdown('<img src ="https://images-na.ssl-images-amazon.com/images/P/0002005018.0" ></img>', unsafe_allow_html=True)
+st.set_page_config(layout="wide")
 
-st.markdown('<img src ="https://images-na.ssl-images-amazon.com/images/P/0195153448.0" ></img>', unsafe_allow_html=True)
+# Headder
+def Headder():
 
-st.markdown('<img src ="https://images-na.ssl-images-amazon.com/images/P/0002005018.0" ></img>', unsafe_allow_html=True)
+    page_bg_img = f"""
+    <style>
+    [data-testid="stAppViewContainer"] > .main {{
+        background-image: url("https://raw.githubusercontent.com/kunal-mallick/Book_Recommendation/main/img/heading.jpg");
+        background-size: 100%;
+        background-position: top;
+        background-repeat: no-repeat;
+        background-attachment: local;
+    }}
+
+    [data-testid="stHeader"] {{
+        background: rgba(0,0,0,0);
+    }}
+
+        </style>
+    """
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+
+# Main
+
+def main():
+    Headder()
+
+    st.markdown("""
+    <style>
+    .big-font {
+    font-size:40px !important;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+
+    st.markdown("<p style='padding-top:20px', class='big-font'> Enter Your User-ID</p>", unsafe_allow_html=True)
+    uid = st.columns(3)
+    user_id = uid[0].text_input(' ')
+    #body()
+    #Water_Checker()
+
+
+if __name__ == '__main__':
+
+    main()
