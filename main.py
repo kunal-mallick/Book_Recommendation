6518763ea523150f80ecb6df0acfe1b63887140f
Kunal Mallick <kunalmallicks3@gmail.com>
2023-06-02 00:09:56 +0530

    Created style.css

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 
+
 # Headder
 def Headder():
 
@@ -33,126 +34,117 @@ def body():
     <style>
     .big-font {
     font-size:25px !important;
+
     }
     </style>
     """, unsafe_allow_html=True)
 
     st.image('https://raw.githubusercontent.com/kunal-mallick/Book_Recommendation/main/img/trending.jpg')
 
-    t3 = st.columns(3)
+    t1,t2,t3,t4,t5 = st.columns(5)
 
-    with t3[0]:
+    with t1:
         
-        t31 = st.columns(2)
+            st.image('http://images.amazon.com/images/P/0195153448.01.LZZZZZZZ.jpg', width=200)
+            st.text("book 1")
+            st.markdown("<p class='big-font'>  book 4 auther</p>", unsafe_allow_html=True)
+            st.markdown("<p class='big-font'>  book 4 publisher</p>", unsafe_allow_html=True)
 
-        t31[0].image('http://images.amazon.com/images/P/0195153448.01.LZZZZZZZ.jpg')
-        with t31[1]:
-            st.markdown("<p class='big-font'>book 1 name</p>", unsafe_allow_html=True)
-            st.markdown("<p class='big-font'>book 1 auther</p>", unsafe_allow_html=True)
-            st.markdown("<p class='big-font'>book 1 publisher</p>", unsafe_allow_html=True)
-
-    with t3[1]:
+    with t2:
         
-        t32 = st.columns(2)
+        t21 = st.container()
 
-        t32[0].image('http://images.amazon.com/images/P/0195153448.01.LZZZZZZZ.jpg')
-        with t32[1]:
-            st.markdown("<p class='big-font'>book 2 name</p>", unsafe_allow_html=True)
-            st.markdown("<p class='big-font'>book 2 auther</p>", unsafe_allow_html=True)
-            st.markdown("<p class='big-font'>book 2 publisher</p>", unsafe_allow_html=True)
-
-    with t3[2]:
-        
-        t33 = st.columns(2)
-
-        t33[0].image('http://images.amazon.com/images/P/0195153448.01.LZZZZZZZ.jpg')
-        with t33[1]:
-            st.markdown("<p class='big-font'>book 3 name</p>", unsafe_allow_html=True)
-            st.markdown("<p class='big-font'>book 3 auther</p>", unsafe_allow_html=True)
-            st.markdown("<p class='big-font'>book 3 publisher</p>", unsafe_allow_html=True)
-
-    st.markdown("<p style='padding-top:10px', class='big-font'></p>", unsafe_allow_html=True)
-
-    t2 = st.columns(3)
-
-    with t2[0]:
-        
-        t21 = st.columns(2)
-
-        t21[0].image('http://images.amazon.com/images/P/0195153448.01.LZZZZZZZ.jpg')
-        with t21[1]:
+        with t21:
+            st.image('http://images.amazon.com/images/P/0195153448.01.LZZZZZZZ.jpg', width=200)
             st.markdown("<p class='big-font'>book 4 name</p>", unsafe_allow_html=True)
             st.markdown("<p class='big-font'>book 4 auther</p>", unsafe_allow_html=True)
             st.markdown("<p class='big-font'>book 4 publisher</p>", unsafe_allow_html=True)
 
-    with t2[1]:
-        
-        t22 = st.columns(2)
+    with t3:
 
-        t22[0].image('http://images.amazon.com/images/P/0195153448.01.LZZZZZZZ.jpg')
-        with t22[1]:
-            st.markdown("<p class='big-font'>book 5 name</p>", unsafe_allow_html=True)
-            st.markdown("<p class='big-font'>book 5 auther</p>", unsafe_allow_html=True)
-            st.markdown("<p class='big-font'>book 5 publisher</p>", unsafe_allow_html=True)
+        t31 = st.container()
+
+        with t31:
+            st.image('http://images.amazon.com/images/P/0195153448.01.LZZZZZZZ.jpg', width=200)
+            st.markdown("<p class='big-font'>book 4 name</p>", unsafe_allow_html=True)
+            st.markdown("<p class='big-font'>book 4 auther</p>", unsafe_allow_html=True)
+            st.markdown("<p class='big-font'>book 4 publisher</p>", unsafe_allow_html=True)
+
+    with t4:
+        
+        t41 = st.container()
+
+        with t41:
+            st.image('http://images.amazon.com/images/P/0195153448.01.LZZZZZZZ.jpg', width=200)
+            st.markdown("<p class='big-font'>book 4 name</p>", unsafe_allow_html=True)
+            st.markdown("<p class='big-font'>book 4 auther</p>", unsafe_allow_html=True)
+            st.markdown("<p class='big-font'>book 4 publisher</p>", unsafe_allow_html=True)
+
+    with t5:
+
+        t51 = st.container()
+
+        with t51:
+            st.image('http://images.amazon.com/images/P/0195153448.01.LZZZZZZZ.jpg', width=200)
+            st.markdown("<p class='big-font'>book 4 name</p>", unsafe_allow_html=True)
+            st.markdown("<p class='big-font'>book 4 auther</p>", unsafe_allow_html=True)
+            st.markdown("<p class='big-font'>book 4 publisher</p>", unsafe_allow_html=True)
 
     st.markdown("<p style = 'padding-top:10px'></p>", unsafe_allow_html=True)
     st.image('https://raw.githubusercontent.com/kunal-mallick/Book_Recommendation/main/img/top5.png')
     
-    t3 = st.columns(3)
+    t1,t2,t3,t4,t5 = st.columns(5)
 
-    with t3[0]:
+    with t1:
         
-        t31 = st.columns(2)
+            st.image('http://images.amazon.com/images/P/0195153448.01.LZZZZZZZ.jpg', width=200)
+            st.markdown("<p class='big-font'>  book 4 name</p>", unsafe_allow_html=True)
+            st.markdown("<p class='big-font'>  book 4 auther</p>", unsafe_allow_html=True)
+            st.markdown("<p class='big-font'>  book 4 publisher</p>", unsafe_allow_html=True)
 
-        t31[0].image('http://images.amazon.com/images/P/0195153448.01.LZZZZZZZ.jpg')
-        with t31[1]:
-            st.markdown("<p class='big-font'>book 1 name</p>", unsafe_allow_html=True)
-            st.markdown("<p class='big-font'>book 1 auther</p>", unsafe_allow_html=True)
-            st.markdown("<p class='big-font'>book 1 publisher</p>", unsafe_allow_html=True)
-
-    with t3[1]:
+    with t2:
         
-        t32 = st.columns(2)
+        t21 = st.container()
 
-        t32[0].image('http://images.amazon.com/images/P/0195153448.01.LZZZZZZZ.jpg')
-        with t32[1]:
-            st.markdown("<p class='big-font'>book 2 name</p>", unsafe_allow_html=True)
-            st.markdown("<p class='big-font'>book 2 auther</p>", unsafe_allow_html=True)
-            st.markdown("<p class='big-font'>book 2 publisher</p>", unsafe_allow_html=True)
-
-    with t3[2]:
-        
-        t33 = st.columns(2)
-
-        t33[0].image('http://images.amazon.com/images/P/0195153448.01.LZZZZZZZ.jpg')
-        with t33[1]:
-            st.markdown("<p class='big-font'>book 3 name</p>", unsafe_allow_html=True)
-            st.markdown("<p class='big-font'>book 3 auther</p>", unsafe_allow_html=True)
-            st.markdown("<p class='big-font'>book 3 publisher</p>", unsafe_allow_html=True)
-
-    st.markdown("<p style='padding-top:10px', class='big-font'></p>", unsafe_allow_html=True)
-
-    t2 = st.columns(3)
-
-    with t2[0]:
-        
-        t21 = st.columns(2)
-
-        t21[0].image('http://images.amazon.com/images/P/0195153448.01.LZZZZZZZ.jpg')
-        with t21[1]:
+        with t21:
+            st.image('http://images.amazon.com/images/P/0195153448.01.LZZZZZZZ.jpg', width=200)
             st.markdown("<p class='big-font'>book 4 name</p>", unsafe_allow_html=True)
             st.markdown("<p class='big-font'>book 4 auther</p>", unsafe_allow_html=True)
             st.markdown("<p class='big-font'>book 4 publisher</p>", unsafe_allow_html=True)
 
-    with t2[1]:
-        
-        t22 = st.columns(2)
+    with t3:
 
-        t22[0].image('http://images.amazon.com/images/P/0195153448.01.LZZZZZZZ.jpg')
-        with t22[1]:
-            st.markdown("<p class='big-font'>book 5 name</p>", unsafe_allow_html=True)
-            st.markdown("<p class='big-font'>book 5 auther</p>", unsafe_allow_html=True)
-            st.markdown("<p class='big-font'>book 5 publisher</p>", unsafe_allow_html=True)
+        t31 = st.container()
+
+        with t31:
+            st.image('http://images.amazon.com/images/P/0195153448.01.LZZZZZZZ.jpg', width=200)
+            st.markdown("<p class='big-font'>book 4 name</p>", unsafe_allow_html=True)
+            st.markdown("<p class='big-font'>book 4 auther</p>", unsafe_allow_html=True)
+            st.markdown("<p class='big-font'>book 4 publisher</p>", unsafe_allow_html=True)
+
+    #st.markdown("<p style='padding-top:10px', class='big-font'></p>", unsafe_allow_html=True)
+
+    with t4:
+        
+        t41 = st.container()
+
+        with t41:
+            st.image('http://images.amazon.com/images/P/0195153448.01.LZZZZZZZ.jpg', width=200)
+            st.markdown("<p class='big-font'>book 4 name</p>", unsafe_allow_html=True)
+            st.markdown("<p class='big-font'>book 4 auther</p>", unsafe_allow_html=True)
+            st.markdown("<p class='big-font'>book 4 publisher</p>", unsafe_allow_html=True)
+
+    with t5:
+
+        t51 = st.container()
+
+        with t51:
+            st.image('http://images.amazon.com/images/P/0195153448.01.LZZZZZZZ.jpg', width=200)
+            st.markdown("<p class='big-font'>book 4 name</p>", unsafe_allow_html=True)
+            st.markdown("<p class='big-font'>book 4 auther</p>", unsafe_allow_html=True)
+            st.markdown("<p class='big-font'>book 4 publisher</p>", unsafe_allow_html=True)
+
+    st.columns(2)
 # Main
 
 def main():
@@ -166,9 +158,11 @@ def main():
     </style>
     """, unsafe_allow_html=True)
 
-    st.markdown("<p style='padding-top:20px', class='big-font'> Enter Your User-ID</p>", unsafe_allow_html=True)
-    uid = st.columns(3)
-    user_id = uid[0].text_input(' ')
+    uid = st.columns([1,3])
+    uid[0].markdown("<p style='padding-top:20px', class='big-font'> Enter Your User-ID</p>", unsafe_allow_html=True)
+    user_id = uid[1].text_input('')
+    with open('style.css') as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
     body()
 
 
